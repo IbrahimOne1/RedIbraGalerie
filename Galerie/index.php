f6bb2f7eca4bb6de2f0e065201ae0abc69391b4a
IbrahimOne1 <ibrahimdahaoui@outlook.com>
2018-01-23 00:35:50 +0100

    start

---
+++ Galerie/index.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Gallerie</title>
+</head>
+<body>
+<div id="navbar">
+
+</div>
+
+<div id="sidebar">
+
+
+</div>
+
+
+<div id="container">
+
+</div>
+
+
+</body>
+</html>
