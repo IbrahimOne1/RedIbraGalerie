95997caf07fffa93d64b6245ce1199855dd57752
redatlm <benaissamedrida@gmail.com>
2018-01-23 00:32:57 +0100

    Update g1.php

--- Galerie/g1.php
+++ Galerie/g1.php
@@ -1,2 +1,23 @@
-//////////////////GALERIE///////
-//echo("hello mister ibra");
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Gallerie</title>
+</head>
+<body>
+<div id="navbar">
+	
+</div>
+
+<div id="sidebar">
+	
+
+</div>
+
+
+<div id="container">
+	
+</div>
+
+
+</body>
+</html>
