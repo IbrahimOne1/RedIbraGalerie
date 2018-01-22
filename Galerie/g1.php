e4013f5f85213114eb9bc524febf045500869698
redatlm <benaissamedrida@gmail.com>
2018-01-23 00:46:10 +0100

    Update g1.php

--- Galerie/g1.php
+++ Galerie/g1.php
@@ -5,16 +5,39 @@
 </head>
 <body>
 <div id="navbar">
-	
+	<a href="">index</a>
+	<a href="">login</a>
+	<a href="">sos tomate</a>
+	<a href="">inscription</a>
 </div>
 
 <div id="sidebar">
-	
+	<div id="login">
+		<form method="POST" action="index.php">
+			<input type="text" name="username" placeholder="username" />
+			<input type="password" name="password" placeholder="password" />
+			<input type="submit" name="submit" value="Login">
+		</form>
+	</div>
 
+	<ul>
+		<li>nature</li>
+		<li>football</li>
+		<li>caraté</li>
+		<li>lmazouzi</li>
+	</ul>
 </div>
 
-
 <div id="container">
+
+	<img src="img/1.jpg" id="img" />
+	<img src="img/2.jpg" id="img" />
+	<img src="img/3.jpg" id="img" />
+	<img src="img/4.jpg" id="img" />
+	<img src="img/5.jpg" id="img" />
+	<img src="img/6.png" id="img" />
+	<img src="img/7.jpg" id="img" />
+	<img src="img/8.jpg" id="img" />
 	
 </div>
 
